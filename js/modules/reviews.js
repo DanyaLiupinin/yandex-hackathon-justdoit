d90803ecb401a780cf5bc25fd73ae1613c07967f
Kliueva-Kath <98223602+Kliueva-Kath@users.noreply.github.com>
2022-11-30 18:56:38 +0300

    feat: начать слайдер для reviews

--- js/modules/reviews.js
+++ js/modules/reviews.js
@@ -1,16 +1,15 @@
-export function slider() {
   $(".responsive").slick({
     dots: true,
     infinite: false,
     speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToShow: 2,
+    slidesToScroll: 2,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 2,
+          slidesToScroll: 2,
           infinite: true,
           dots: true,
         },
@@ -34,4 +33,7 @@ export function slider() {
       // instead of a settings object
     ],
   });
-}
+
+
+$(".reviews__back-button");.slick("slickPrev");
+$(".reviews__forward-button").slick("slickNext");
