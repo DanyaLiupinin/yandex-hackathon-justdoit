922a95f4226b1cb13912283b3697ff45586e8ec7
Kliueva-Kath <98223602+Kliueva-Kath@users.noreply.github.com>
2022-12-02 00:17:49 +0300

    fix: правки от дизайнеров

--- js/quiz.js
+++ js/quiz.js
@@ -1,4 +1,4 @@
-import girl_image from '../images/Pic_girl.svg';
+import girl_image from "../images/Pic_girl.svg";
 
 let numb = 1;
 
@@ -15,7 +15,8 @@ const quizArray = [
   },
   {
     id: "3",
-    question: "2. Если бы ты был учителем, то что тебе больше хотелось: читать лекции или проверять домашние задания?",
+    question:
+      "2. Если бы ты был учителем, то что тебе больше хотелось: читать лекции или проверять домашние задания?",
     options: ["читать лекции", "проверять домашние задания"],
     replies: [4, 5],
   },
@@ -39,53 +40,53 @@ const quizArray = [
   },
 ];
 
-
-
 let initialize = (numb) => {
-	let test_subtitle = document.getElementById("test_subtitle");
-	test_subtitle.innerHTML = `Вопрос ${Math.ceil(numb/2)} из 5`;
-	if (numb!=1){
-		let dot = document.getElementById(`test_dot${Math.ceil(numb/2)}`);
-		dot.style.backgroundColor = "#9D85F4";	
-	}
+  let test_subtitle = document.getElementById("test_subtitle");
+  test_subtitle.innerHTML = `Вопрос ${Math.ceil(numb / 2)} из 5`;
+  if (numb != 1) {
+    let dot = document.getElementById(`test_dot${Math.ceil(numb / 2)}`);
+    dot.style.backgroundColor = "#9D85F4";
+  }
 
-	quiz_wrapper.innerHTML += `<button id="quiz_Question${numb}" class="question"></button>`
-	quiz_wrapper.innerHTML += `<button id="quiz_reply${numb}" class="reply"></button>`
-	quiz_wrapper.innerHTML += `<button id="quiz_reply${numb+1}" class="reply"></button>`
-  quiz_wrapper.innerHTML += `<button id="quiz_focuser${numb}" class="reply_focuser"></button>`
+  quiz_wrapper.innerHTML += `<button id="quiz_Question${numb}" class="question"></button>`;
+  quiz_wrapper.innerHTML += `<button id="quiz_reply${numb}" class="reply"></button>`;
+  quiz_wrapper.innerHTML += `<button id="quiz_reply${
+    numb + 1
+  }" class="reply"></button>`;
+  quiz_wrapper.innerHTML += `<button id="quiz_focuser${numb}" class="reply_focuser"></button>`;
 
-	let quizQuestion = document.getElementById(`quiz_Question${numb}`);
-	let quizReply1 = document.getElementById(`quiz_reply${numb}`);
-	let quizReply2 = document.getElementById(`quiz_reply${numb+1}`);
-  
-  quiz_wrapper.scrollTop=quiz_wrapper.scrollHeight;
+  let quizQuestion = document.getElementById(`quiz_Question${numb}`);
+  let quizReply1 = document.getElementById(`quiz_reply${numb}`);
+  let quizReply2 = document.getElementById(`quiz_reply${numb + 1}`);
+
+  quiz_wrapper.scrollTop = quiz_wrapper.scrollHeight;
 
   let repliesArray = [];
-	const result = quizArray.filter(obj => obj.id == numb);
+  const result = quizArray.filter((obj) => obj.id == numb);
 
-	  quizQuestion.innerHTML = result[0].question;
-  	quizReply1.innerHTML = result[0].options[0];
-  	quizReply2.innerHTML = result[0].options[1];
+  quizQuestion.innerHTML = result[0].question;
+  quizReply1.innerHTML = result[0].options[0];
+  quizReply2.innerHTML = result[0].options[1];
 
-	  quizReply1.addEventListener("click", (e) => {
-		e.preventDefault();
-		quizReply2.classList.add("hidden");
-		quizReply1.style.backgroundColor = "#FEF9D8";
+  quizReply1.addEventListener("click", (e) => {
+    e.preventDefault();
+    quizReply2.classList.add("hidden");
+    quizReply1.style.backgroundColor = "#FEF9D8";
     quizReply1.style.border = "none";
     repliesArray.push(result[0].options[0]);
-		if (numb<9) initialize (numb+2,numb+3)
+    if (numb < 9) initialize(numb + 2, numb + 3);
     else sendToServer(repliesArray);
-	})
-	quizReply2.addEventListener("click", (e) => {
-		e.preventDefault();
-		quizReply1.classList.add("hidden");
-		quizReply2.style.backgroundColor = "#FEF9D8";	
+  });
+  quizReply2.addEventListener("click", (e) => {
+    e.preventDefault();
+    quizReply1.classList.add("hidden");
+    quizReply2.style.backgroundColor = "#FEF9D8";
     quizReply2.style.border = "none";
     repliesArray.push(result[0].options[1]);
-		if (numb<9) initialize (numb+2,numb+3)
+    if (numb < 9) initialize(numb + 2, numb + 3);
     else sendToServer(repliesArray);
-	})
-}
+  });
+};
 
 initialize(1);
 var timer = null;
@@ -97,15 +98,16 @@ const sendToServer = async (results) => {
     const resultData = results;
     if (!resultData) {
       throw new Error("Invalid data");
-    }    
-  } catch (error) {console.log(`Ошибка: ${error}`)}
-       finally {
-      setTimeout(() => {
-        loading(false);
-        showResult (); //showResult (resultData);
-      }, 5000) 
+    }
+  } catch (error) {
+    console.log(`Ошибка: ${error}`);
+  } finally {
+    setTimeout(() => {
+      loading(false);
+      showResult(); //showResult (resultData);
+    }, 500);
   }
-}
+};
 
 const loading = (loading) => {
   if (loading) {
@@ -121,13 +123,13 @@ const loading = (loading) => {
       <h2 class="load_title">Подсчет результатов...</h2>
       </div>
       `;
-    loader (true);
-}
-else {
-  loader(false)};
-}
+    loader(true);
+  } else {
+    loader(false);
+  }
+};
 
-const showResult = () => { 
+const showResult = () => {
   test_popup.innerHTML = `
   <h2 class="title is-2 quiz_final_title">Тебе бы подошла роль ревьюера!</h2>
 
@@ -148,37 +150,38 @@ const showResult = () => {
   <button href="#" class="quiz_button_to_vac" id="quiz_button_to_vac">Открыть список вакансий</button>
   <button href="#" class="quiz_button_to_descr" id="quiz_button_to_descr">Вернуться к описанию профессий</button>
   `;
-  let button_vac = document.getElementById('quiz_button_to_vac');
-  let button_descr = document.getElementById('quiz_button_to_descr');
+  let button_vac = document.getElementById("quiz_button_to_vac");
+  let button_descr = document.getElementById("quiz_button_to_descr");
 
   button_vac.addEventListener("click", (e) => {
     e.preventDefault();
-    test_popup__bg.classList.remove('active');
-  }
-  )
+    test_popup__bg.classList.remove("active");
+  });
   button_descr.addEventListener("click", (e) => {
     e.preventDefault();
-    test_popup__bg.classList.remove('active');
-  }
-  )
-}
+    test_popup__bg.classList.remove("active");
+  });
+};
 
 const loader = (state) => {
-
   if (!state) {
     clearInterval(timer);
     return;
-   }
+  }
 
   let i = 1;
   timer = setInterval(() => {
     let nextDot = document.getElementById(`load_dot${i++}`);
     nextDot.style.backgroundColor = "#9D85F4";
-    if (i == 2) {let prevDot = document.getElementById(`load_dot5`)
-    prevDot.style.backgroundColor = "#DCD3FB";}
-    else {let prevDot = document.getElementById(`load_dot${i-2}`)
-    prevDot.style.backgroundColor = "#DCD3FB";};
-    if ( i == 6 ) {i = 1};
-  }, 1000)
- }
-
+    if (i == 2) {
+      let prevDot = document.getElementById(`load_dot5`);
+      prevDot.style.backgroundColor = "#DCD3FB";
+    } else {
+      let prevDot = document.getElementById(`load_dot${i - 2}`);
+      prevDot.style.backgroundColor = "#DCD3FB";
+    }
+    if (i == 6) {
+      i = 1;
+    }
+  }, 1000);
+};
